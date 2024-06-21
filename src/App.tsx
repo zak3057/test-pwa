f76eeec6922aa12aa05a8afab5d603197e80c1d7
zak <zak@example.com>
2024-06-21 19:10:41 +0900

    ローカルプッシュ通知実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,58 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const askNotificationPermission = () => {
+  console.log('askNotificationPermission 1');
+  return new Promise((resolve) => {
+    if (Notification.permission === 'granted') {
+      console.log('askNotificationPermission 2');
+      resolve('granted');
+    } else if (Notification.permission === 'denied') {
+      console.log('askNotificationPermission 3');
+      resolve('denied');
+    } else {
+      console.log('askNotificationPermission 4');
+      Notification.requestPermission().then(permission => {
+        console.log('askNotificationPermission 5');
+        resolve(permission);
+      });
+    }
+  });
+}
 
+const sendNotification = async () => {
+  console.log('sendNotification s');
+ 
+  await new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 10000);
+  })
+
+  if (Notification.permission === 'granted') {
+    navigator.serviceWorker.getRegistration().then(registration => {
+      console.log('sendNotification then');
+      if (registration) {
+        registration.showNotification('Hello World!', {
+          body: 'This is a test notification',
+        });
+      }
+    });
+  }
+}
+
+function App() {
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => askNotificationPermission()}>
+          通知許可
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <button onClick={() => sendNotification()}>
+          local push
+        </button>
+      </div>
     </>
   )
 }
